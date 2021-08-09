f5965bbf9c0ada9475eefeee639a9a9b78a15023
joshua-sass <josh.sass@sassware.com>
2021-08-09 15:39:11 -0500

    near the end of board generation

--- board.py
+++ board.py
@@ -3,23 +3,16 @@ from random import randint
 
 class board:
 	def __init__(self):
-		#randomly generate numbers that add up to 100
 		#determine tiles and values here!
-		self.chits = [2,3,3,4,4,5,5,6,6,8,8,9,9,10,10,11,11,12]
+		self.chits = []
 		self.harbors = ["s", "?", "?", "b", "?", "w", "g", "?", "r"]
+		self.tiles = []
 		#d = desert
 		#w = wood
 		#r = rock
 		#g = grain
 		#s = sheep
 		#b = brick
-		self.tiles = []
-		desert_tile = tuple(["d", 0])
-		self.tiles.append(desert_tile)
-
-		self.generate_chit_values()
-
-		print(self.tiles)
 
 	def generate_chit_values(self): #(tuning these values) #potentially revisit this section to make code cleaner
 		grain_values = []
@@ -28,10 +21,10 @@ class board:
 		rock_values = []
 		brick_values = []
 
+		self.chits = [2,3,3,4,4,5,5,6,6,8,8,9,9,10,10,11,11,12]
 		moving_chits = self.chits
 
 		while len(moving_chits) > 3:
-			#print(self.chits)
 			temp = randint(0, len(moving_chits)-1)
 			grain_values.append(moving_chits[temp])
 			moving_chits.remove(moving_chits[temp])
@@ -52,7 +45,6 @@ class board:
 			brick_values.append(moving_chits[temp])
 			moving_chits.remove(moving_chits[temp])
 
-		#print(self.chits)
 		temp = randint(0, len(moving_chits)-1)
 		grain_values.append(moving_chits[temp])
 		moving_chits.remove(moving_chits[temp])
@@ -64,10 +56,9 @@ class board:
 		temp = randint(0, len(moving_chits)-1)
 		pasture_values.append(moving_chits[temp])
 		moving_chits.remove(moving_chits[temp])
-		#print(self.chits)
 
 		for iterator in range(0, 3):
-			print(len(self.tiles))
+
 			self.tiles.append(tuple(["g", grain_values[iterator]]))
 			self.tiles.append(tuple(["w", wood_values[iterator]]))
 			self.tiles.append(tuple(["s", pasture_values[iterator]]))
@@ -89,12 +80,19 @@ class board:
 		while answer != "y":
 			self.generate_board()
 			print("Does this board look satisfactory? (y,n)")
-			answer = input()
+			answer = input().lower()
 			if answer == "y":
 				break
 			print("Okay! Let's try again")
 
 
 	def generate_board(self):
-		pass
+
+		self.tiles = []
+		desert_tile = tuple(["d", 0])
+		self.tiles.append(desert_tile)
+
+		self.generate_chit_values()
+		self.tiles.sort()
+		print(self.tiles)
 		#numeric system so tiles can be stored in an array (tile #x is always in y location)
