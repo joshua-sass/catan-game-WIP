41fbd2602979269f3ec9207d83efb8b33a870702
joshua-sass <josh.sass@sassware.com>
2021-08-08 22:12:47 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+
+
+if __name__ == "__main__":
+	#draw main screen
+
+	#player number (fills out with ai)
+	# - 2 to 5 total players
+	# type number and press enter. check input
+
+	#offer difficulty selection
+	# - easy, ai makes decision based on top 5 options, memory buffer of 1 round
+	# - medium, ai makes decision based on top 3 options, memory buffer of 3 rounds
+	# - hard, ai makes decision based on best option, memory buffer is all cards in hands
+	# type selection and press enter. check input
+
+	#credits
+	#press enter to continue or type rules for rulebook (can use browser to open webpage)
+
+	#create game object and begin
